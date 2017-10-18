4c12c3c6b5376b4fda2bae8af58faff796f6c3a6
rajatrawat <rajatrawataku1@gmail.com>
2017-10-18 09:23:28 +0530

    Added js files

---
+++ frontend/js/index.js
@@ -0,0 +1,9 @@
+// var Bing = require('node-bing-api')({ accKey: "eda9d2513af94fbb829b331f72e6b30a" });
+//
+// Bing.news("Aadhar nandan India", {
+//     count: 10  // Number of results (max 15)
+//        // Skip first 3 results
+//   },function(error, res, body){
+//     for(var i=0;i<10;i++)
+//     console.log(body.value[i]);
+//   });
